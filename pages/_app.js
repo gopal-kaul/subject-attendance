95ea6af13f7afbad6b51444a2d4892ed4f5292e4
Gopal Kaul <gopalkaul5@gmail.com>
2022-10-24 14:55:09 +0530

    Add vertical padding

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="mx-4 md:mx-20 lg:mx-60">
+    <div className="mx-4 md:mx-20 lg:mx-60 my-[10vh]">
       <Component {...pageProps} />
     </div>
   );
